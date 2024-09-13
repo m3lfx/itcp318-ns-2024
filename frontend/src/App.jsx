7f8fae694e68a1f7d3b44be2777aad784f164556
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-13 11:47:49 +0800

    home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
 import Header from './Components/Layout/Header'
-
+import Footer from './Components/Layout/Footer'
+import Home from './Components/Home';
 function App() {
   return (
     <>
-     <Header />
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+        </Routes>
+
+      </Router>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import MetaData from './Layout/MetaData'
+import axios from 'axios'
+import Product from './Product/Product'
+
+
+
+const Home = () => {
+    const [products, setProducts] = useState([])
+    const [productsCount, setProductsCount] = useState(0)
+    const getProducts = async () => {
+
+        let link = `http://localhost:4001/api/v1/products`
+        // link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}&price[lte]=${price[1]}&price[gte]=${price[0]}`
+
+
+        let res = await axios.get(link)
+        console.log(res)
+        setProducts(res.data.products)
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, []);
+    return (
+        <>
+            <MetaData title={'Buy Best Products Online'} />
+            <div className="container container-fluid">
+                <h1 id="products_heading">Latest Products</h1>
+                <section id="products" className="container mt-5">
+                    <div className="row">
+                        {products && products.map(product => (
+                            <Product key={product._id} product={product} />
+                        ))}
+
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Home

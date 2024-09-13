426b2a18c8ef7bb8cdb65b62411b791dfd73a0e1
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-13 11:11:14 +0800

    get products

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-// const products = require('./routes/product');
+const products = require('./routes/product');
 
 
 
@@ -9,7 +9,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cors());
 
-// app.use('/api/v1', products);
+app.use('/api/v1', products);
 
 
 module.exports = app
--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,11 +1,12 @@
 const Product = require('../models/product')
 exports.getProducts = async (req, res, next) => {
     const products = await Product.find();
+    const productsCount = await Product.countDocuments();
 	if (!products) 
         return res.status(400).json({message: 'error loading products'})
    return res.status(200).json({
         success: true,
-        count: products.count,
+        count: productsCount,
         products,
 		
     })

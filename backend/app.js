73c32164ef6af12dd70ff9f686f55259d6ee6af6
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-20 11:12:55 +0800

    register api

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const products = require('./routes/product');
-
+const auth = require('./routes/auth');
 
 
 app.use(express.json({limit:'50mb'}));
@@ -10,6 +10,6 @@ app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cors());
 
 app.use('/api/v1', products);
-
+app.use('/api/v1', auth);
 
 module.exports = app

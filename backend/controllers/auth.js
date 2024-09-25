c4ce8c5707c544159e012d6429d0ca5f2210648d
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-25 09:53:29 +0800

    reset password token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const crypto = require('crypto')
 const cloudinary = require('cloudinary')
 
+const sendEmail = require('../utils/sendEmail')
 
 
 exports.registerUser = async (req, res, next) => {
@@ -25,13 +26,13 @@ exports.registerUser = async (req, res, next) => {
         },
     })
     //test token
-     const token = user.getJwtToken();
+    const token = user.getJwtToken();
 
-     return res.status(201).json({
-      	success:true,
-      	user,
-     	token
-      })
+    return res.status(201).json({
+        success: true,
+        user,
+        token
+    })
     // sendToken(user, 200, res)
 }
 
@@ -42,31 +43,63 @@ exports.loginUser = async (req, res, next) => {
     if (!email || !password) {
         return res.status(400).json({ error: 'Please enter email & password' })
     }
-    
+
 
     // Finding user in database
-    
+
     let user = await User.findOne({ email }).select('+password')
     if (!user) {
         return res.status(401).json({ message: 'Invalid Email or Password' })
     }
-   
+
 
     // Checks if password is correct or not
     const isPasswordMatched = await user.comparePassword(password);
 
-   
+
     if (!isPasswordMatched) {
         return res.status(401).json({ message: 'Invalid Email or Password' })
     }
     const token = user.getJwtToken();
 
-     res.status(201).json({
-     	success:true,
-
-     	token,
+    res.status(201).json({
+        success: true,
+        token,
         user
-     });
+    });
     //  user = await User.findOne({ email })
     // sendToken(user, 200, res)
+}
+
+exports.forgotPassword = async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(404).json({ error: 'User not found with this email' })
+      
+    }
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+    // Create reset password url
+    const resetUrl = `${req.protocol}://localhost:4001/password/reset/${resetToken}`;
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'ShopIT Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({ validateBeforeSave: false });
+        return res.status(500).json({ error: error.message })
+        // return next(new ErrorHandler(error.message, 500))
+    }
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const upload = require("../utils/multer");
-const { registerUser, loginUser } = require('../controllers/auth');
+const { registerUser, loginUser, forgotPassword } = require('../controllers/auth');
 
 router.post('/register', upload.single("avatar"), registerUser);
 router.post('/login', loginUser);
+router.post('/password/forgot', forgotPassword);
 module.exports = router;

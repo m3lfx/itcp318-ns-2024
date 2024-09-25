ea128fe69055338820e48a94b40cfd5496f474d9
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-25 08:43:03 +0800

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './Components/Layout/Footer'
 import Home from './Components/Home';
 import ProductDetails from './Components/Product/ProductDetails';
 import Register from './Components/User/Register';
+import Login from './Components/User/Login';
 function App() {
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home />} exact="true" />
           <Route path="/product/:id" element={<ProductDetails />} exact="true" />
           <Route path="/search/:keyword" element={<Home />} exact="true" />
+          <Route path="/login" element={<Login />} exact="true" />
           <Route path="/register" element={<Register exact="true" />} />
         </Routes>
 
--- frontend/src/Components/Layout/Header.jsx
+++ frontend/src/Components/Layout/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../../App.css'
 import Search from './Search'
 const Header = () => {
@@ -13,8 +14,8 @@ const Header = () => {
 				<Search />
 			
 				<div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-					<button className="btn" id="login_btn">Login</button>
-
+					{/* <button className="btn" id="login_btn">Login</button> */}
+					<Link to="/login" className="btn ml-4" id="login_btn">Login</Link>
 					<span id="cart" className="ml-3">Cart</span>
 					<span className="ml-1" id="cart_count">2</span>
 				</div>

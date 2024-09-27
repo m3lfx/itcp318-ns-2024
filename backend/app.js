2e626f650fede5d20d6f04833f6b4661d47cb811
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-27 11:20:58 +0800

    add to cart

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order')
 
 
 app.use(express.json({limit:'50mb'}));
@@ -11,5 +12,6 @@ app.use(cors());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
+app.use('/api/v1', order);
 
 module.exports = app
